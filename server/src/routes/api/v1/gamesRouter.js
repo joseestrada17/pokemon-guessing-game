86d9861b72b2c6ab27c99375bc15992f72f82c91
jmea17 <jm.ea@hotmail.com>
2023-06-07 12:23:34 -0400

    Completed serializers for games and prompts

--- server/src/routes/api/v1/gamesRouter.js
+++ server/src/routes/api/v1/gamesRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { Game, Prompt } from "../../../models/index.js";
+import GamesSerializer from "../../../serializers/gamesSerializer.js";
 
 const gamesRouter = new express.Router();
 
 gamesRouter.get("/", async (req, res) => {
   try {
     const games = await Game.query();
-    return res.status(200).json({ games });
+    const serializedGames = games.map((game) => GamesSerializer.serialize(game));
+    return res.status(200).json({ games: serializedGames });
   } catch (error) {
     res.status(500).json({ errors: error.message });
   }
@@ -22,22 +24,24 @@ gamesRouter.get("/:id", async (req, res) => {
     }
 
     const prompts = await Prompt.query().where("gameId", id);
+    const serializedData = GamesSerializer.serializeWithPrompts(game, prompts);
 
-    return res.status(200).json({ game, prompts });
+    return res.status(200).json(serializedData);
   } catch (error) {
     res.status(500).json({ errors: error.message });
   }
 });
 
 gamesRouter.post("/", async (req, res) => {
-  const { body } = req;
+  const { body, user } = req;
   const formInput = body;
   const { title } = formInput;
-  const userId = req.user.id;
+  const userId = user.id;
   try {
     const newGame = await Game.query().insertAndFetch({ title, userId });
+    const serializedGame = GamesSerializer.serialize(newGame);
 
-    return res.status(201).json({ newGame });
+    return res.status(201).json(serializedGame);
   } catch (error) {
     if (error instanceof ValidationError) {
       res.status(422).json({ errors: error.data });
